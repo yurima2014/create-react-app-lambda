e59644f22091fe3798305eb540aeabc205bddf20
Yuri <yuri.maximov@mail.huji.ac.il>
2022-04-10 11:59:53 +0300

    Added progress counter, removed 'colsole.log()'s, removed commented code.

--- src/App.js
+++ src/App.js
@@ -1,216 +1,151 @@
-import React, { Component } from "react"
+import React, {Component} from "react"
 import logo from "./logo.svg"
 import "./App.css"
 
-
-
-/*
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
-
-*/
-
-
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.questionsList = props.data;
-    this.index = 0;
-    this.score = 0;
-    this.handleClickCorrect = this.handleClickCorrect.bind(this);
-    this.handleClickIncorrect = this.handleClickIncorrect.bind(this);
-    //this.handleTimedOutIncorrect = this.handleTimedOutIncorrect(this);
-    this.proceedToNextQuestion = this.proceedToNextQuestion.bind(this);
+    constructor(props) {
+        super(props);
+        this.questionsList = props.data;
+        this.index = 0;
+        this.score = 0;
+        this.handleClickCorrect = this.handleClickCorrect.bind(this);
+        this.handleClickIncorrect = this.handleClickIncorrect.bind(this);
+        this.proceedToNextQuestion = this.proceedToNextQuestion.bind(this);
 
-    this.timer = 0;
-  }
+        this.timer = 0;
+    }
 
 
-  handleClickCorrect(){
-    console.log('Correct answer');
+    handleClickCorrect() {
 
-    switch (this.questionsList[this.index].difficulty) {
+        switch (this.questionsList[this.index].difficulty) {
 
-      case 'easy': this.score += 1; break;
-      case 'meadium': this.score += 2; break;
-      case 'hard': this.score += 3; break;
-        default: break;
+            case 'easy':
+                this.score += 1;
+                break;
+            case 'medium':
+                this.score += 2;
+                break;
+            case 'hard':
+                this.score += 3;
+                break;
+            default:
+                break;
 
+        }
+        ;
+
+        this.proceedToNextQuestion();
     };
 
-    this.proceedToNextQuestion();
-  };
+
+    handleClickIncorrect() {
+        this.proceedToNextQuestion();
+    };
 
 
-  handleClickIncorrect(){
-    console.log('Incorrect answer');
-    this.proceedToNextQuestion();
-  };
-
-  /*
-  handleTimedOutIncorrect(){
-    clearTimeout(this.timer);
-    this.proceedToNextQuestion();
-  };*/
-
-  proceedToNextQuestion(){
-    clearTimeout(this.timer);
-    this.index++;
-    this.forceUpdate();
-
-  };
+    proceedToNextQuestion() {
+        clearTimeout(this.timer);
+        this.index++;
+        this.forceUpdate();
+    };
 
 
+    render() {
 
-  render() {
+        let currQuestion = this.questionsList[this.index];
+        this.timer = setTimeout(this.proceedToNextQuestion, 5000);
 
-    let currQuestion = this.questionsList[this.index];
-    this.timer = setTimeout(this.proceedToNextQuestion, 5000);
-
-
-    if(this.index === this.questionsList.length)
-    {
-      clearTimeout(this.timer);
-      return(
-
-
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Updated <code>src/App.js</code>  as initial commit.
-
-              </p>
-
-
-              <p> Congratulations, you completed the quiz! Your score is {this.score} points</p>
-
-
-
-
-
-
-            </header>
-          </div>
-      )
-    }
-
-
-    switch(currQuestion.type) {
-
-
-
-      case 'multiple':
-        return (
-            <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Updated <code>src/App.js</code>  as initial commit.
-
-                </p>
-
-
-
-                <div>
-
-                  <p>Category: {currQuestion.category}</p>
-                <p>{currQuestion.question}</p>
-
-                <button onClick={this.handleClickCorrect}> {currQuestion.correct_answer} </button>
-                <button onClick={this.handleClickIncorrect}> {currQuestion.incorrect_answers[0]} </button>
-                <button onClick={this.handleClickIncorrect}> {currQuestion.incorrect_answers[1]} </button>
-                <button onClick={this.handleClickIncorrect}> {currQuestion.incorrect_answers[2]} </button>
-
-
+        if (this.index === this.questionsList.length) {
+            clearTimeout(this.timer);
+            return (
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <p>Progress: {this.index} / {this.questionsList.length} questions </p>
+                        <p> Congratulations, you completed the quiz! Your score is {this.score} points</p>
+                    </header>
                 </div>
+            )
+        }
 
 
+        switch (currQuestion.type) {
+
+            case 'multiple':
+                return (
+                    <div className="App">
+                        <header className="App-header">
+                            <img src={logo} className="App-logo" alt="logo"/>
+                            <p>Progress: {this.index} / {this.questionsList.length} questions </p>
+                            <p> Score: {this.score}</p>
+
+                            <div>
+
+                                <p>Category: {currQuestion.category}</p>
+                                <p>{currQuestion.question.replace(/&quot;/g, '"').replace(/&#039;/g, "'")}</p>
+
+                                <button onClick={this.handleClickCorrect}> {currQuestion.correct_answer} </button>
+                                <button
+                                    onClick={this.handleClickIncorrect}> {currQuestion.incorrect_answers[0]} </button>
+                                <button
+                                    onClick={this.handleClickIncorrect}> {currQuestion.incorrect_answers[1]} </button>
+                                <button
+                                    onClick={this.handleClickIncorrect}> {currQuestion.incorrect_answers[2]} </button>
 
 
-
-              </header>
-            </div>
-        )
+                            </div>
 
 
-      case 'boolean':
-
-        return (
-            <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Updated <code>src/App.js</code>  as initial commit.
-
-                </p>
+                        </header>
+                    </div>
+                )
 
 
+            case 'boolean':
 
-                <div>
+                return (
+                    <div className="App">
+                        <header className="App-header">
+                            <img src={logo} className="App-logo" alt="logo"/>
+                            <p>Progress: {this.index} / {this.questionsList.length} questions </p>
+                            <p> Score: {this.score}</p>
+                            <div>
 
-                  <p>Category: {currQuestion.category}</p>
-                  <p>{currQuestion.question}</p>
+                                <p>Category: {currQuestion.category}</p>
+                                <p>{currQuestion.question}</p>
 
-                  <button onClick={this.handleClickCorrect}> {currQuestion.correct_answer} </button>
-                  <button onClick={this.handleClickIncorrect}> {currQuestion.incorrect_answers[0]} </button>
-
-
-                </div>
-
-
-
-
-
-              </header>
-            </div>
-        )
+                                <button onClick={this.handleClickCorrect}> {currQuestion.correct_answer} </button>
+                                <button
+                                    onClick={this.handleClickIncorrect}> {currQuestion.incorrect_answers[0]} </button>
 
 
-        default: return(
-            <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Updated <code>src/App.js</code>  as initial commit.
-
-                </p>
+                            </div>
 
 
-              </header>
-            </div>
-        )
+                        </header>
+                    </div>
+                )
+
+
+            default:
+                return (
+                    <div className="App">
+                        <header className="App-header">
+                            <img src={logo} className="App-logo" alt="logo"/>
+                            <p>
+                                Updated <code>src/App.js</code> as initial commit.
+                            </p>
+
+
+                        </header>
+                    </div>
+                )
+        }
+
+
     }
-
-
-  }
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -13,22 +13,13 @@ window.onload = function()
     fetch(contentUrl)
         .then(response => response.json())
         .then((jsonData) => {
-            // jsonData is parsed json object received from url
-            console.log(jsonData.results);
             ReactDOM.render(<App data={jsonData.results}/>, document.getElementById('root'));
-            //ReactDOM.render(<Question data={jsonData.results[0]} indexStart={indexStart}/>, document.getElementById('question'));
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
             serviceWorker.unregister()
         })
         .catch((error) => {
-            // handle your errors here
             console.error(error)
         });
 
 };
 
-;
+
